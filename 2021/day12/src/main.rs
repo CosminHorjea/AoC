e82f560510d302d0494c39a40e5958c02f427cdb
Horjea Cosmin <horjeacosmin@gmail.com>
2021-12-12 14:36:33 +0200

    day 12, a little bit too slow for part 2 tho

--- 2021/day12/src/main.rs
+++ 2021/day12/src/main.rs
@@ -1,7 +1,7 @@
-use std::collections::{HashMap, HashSet};
+use std::collections::{HashMap, HashSet, VecDeque};
 
 fn main() {
-    let content = include_str!("day12.test");
+    let content = include_str!("day12.in");
     let mut graph: HashMap<&str, HashSet<&str>> = HashMap::new();
     for line in content.lines() {
         let mut parts = line.split("-");
@@ -10,29 +10,37 @@ fn main() {
         graph.entry(key).or_insert(HashSet::new()).insert(value);
         graph.entry(value).or_insert(HashSet::new()).insert(key);
     }
-    //print all paths from start to end
-    let mut sttack: Vec<(&str)> = vec![("start")];
-    let mut paths: Vec<Vec<&str>> = Vec::new();
-    let mut visited: HashSet<&str> = HashSet::new();
-    while let Some((key)) = sttack.pop() {
-        visited.insert(key);
-        if key == "end" {
-            println!("{:?}", 1);
-            // for path in paths.iter() {
-            //     println!("{:?}", path);
-            // }
-            paths.push(sttack.clone().iter().rev().map(|x| *x).collect());
+    let mut queue: VecDeque<Vec<&str>> = VecDeque::new();
+    let mut path = Vec::new();
+    let mut total_paths:i128 = 0;
+    path.push("start");
+    queue.push_back(path);
+    /**
+     * This helped a lot https://www.geeksforgeeks.org/print-paths-given-source-destination-using-bfs/
+     */
+
+    while(queue.len() > 0) {
+        path = queue.pop_front().unwrap();
+        let key = path.last().unwrap();
+        if *key == "end" {
+            // println!("{:?}", path);
+            total_paths+=1;
+            // already_visited = false;
         } else {
             for value in graph.get(key).unwrap() {
-                if !visited.contains(value) || !value.chars().nth(0).unwrap().is_lowercase() {
-                    sttack.push((value));
+                //Part 2 is slow a f, like 5 min for result
+                if !(path.iter().filter(|x| x.chars().nth(0).unwrap().is_lowercase()).collect::<HashSet<&&str>>().len()+1 <
+                path.iter().filter(|x| x.chars().nth(0).unwrap().is_lowercase()).collect::<Vec<&&str>>().len() ) || !value.chars().nth(0).unwrap().is_lowercase() {
+                    if(*value == "start"){
+                        continue;
+                    }
+                    let mut new_path = path.clone();
+                    new_path.push(*value);
+                    queue.push_back(new_path);
                 }
             }
         }
     }
-
-    //print paths
-    for path in paths.iter() {
-        println!("{:?}", path);
-    }
+    // println!("Part 1 {:?}", total_paths);
+    println!("Part 2 {:?}", total_paths);
 }
