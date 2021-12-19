97f13f733cbf2dfabe2e49424d10fd8076281dda
CosminHorjea <horjeacosmin@gmail.com>
2021-12-19 11:37:19 +0200

    day 18, ashamed and happy at the same time

--- day18/sol.py
+++ day18/sol.py
@@ -1,4 +1,22 @@
-f = open("day18.test", "r")
+f = open("day18.in", "r")
+
+
+def calculate(nums):
+    depth = 4
+    while depth > 0:
+        sol = 0
+        for i in range(len(nums)):
+            if nums[i][1] == depth:
+                x, y = nums[i][0], nums[i + 1][0]
+                nums.pop(i)
+                nums.pop(i)
+                nums.insert(i, (3 * x + 2 * y, depth - 1))
+                sol = 1
+                break
+        if not sol:
+            depth -= 1
+
+    return nums[0][0]
 
 
 def solve(nums):
@@ -7,9 +25,8 @@ def solve(nums):
     while not solved:
         solved = 1
 
-        print("Step: ", nums)
+        # print("Step: ", nums)
         for (i, (num, depth)) in enumerate(nums):
-
             if depth > 4:
                 if i > 0:
                     (n, m) = nums[i - 1]
@@ -22,6 +39,9 @@ def solve(nums):
                 nums.insert(i, (0, depth - 1))
                 solved = 0
                 break
+        if not solved:
+            continue
+        for (i, (num, depth)) in enumerate(nums):
             if num >= 10:
                 nums.pop(i)
                 nums.insert(i, (num // 2, depth + 1))
@@ -31,25 +51,42 @@ def solve(nums):
     return nums
 
 
-nums = []
-for line in f:
-    print("Init: ", nums)
-    print("Line: ", line.strip())
-    for i in range(len(nums)):
-        nums[i] = (nums[i][0], nums[i][1] + 1)
-    depth = len(nums) > 1
-    for c in line:
+max_calc = 0
+
+lines = f.readlines()
+for line1 in lines:
+    depth = 0
+    nums = []
+    for c in line1:
         if c == "[":
             depth += 1
         elif c.isdigit():
             nums.append((int(c), depth))
         elif c == "]":
             depth -= 1
-    print("Before: ", nums)
-    nums = solve(nums)
+    for line2 in lines:
+        nums_aux = nums.copy()
+        # print("Init: ", nums)
+        # print("Line: ", line.strip())
+        for i in range(len(nums)):
+            nums[i] = (nums[i][0], nums[i][1] + 1)
+        depth = len(nums) > 1
+        for c in line2:
+            if c == "[":
+                depth += 1
+            elif c.isdigit():
+                nums.append((int(c), depth))
+            elif c == "]":
+                depth -= 1
+        # print("Before: ", nums)
+        nums = solve(nums)
 
-    print("After: ", nums)
-    print()
+        # print("After: ", nums)
+        # print()
+        print(calculate(nums))
+
+        max_calc = max(calculate(nums), max_calc)
+        nums = nums_aux.copy()
 
 
-# print(nums)
+print("Max: ", max_calc)
