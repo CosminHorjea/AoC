d0c0e28b94e1c07ac11c18610a93a4d8056d48cf
Horjea Cosmin <horjeacosmin@gmail.com>
2021-12-22 20:21:36 +0200

    day 19 progress

--- 2021/day19/sol.py
+++ 2021/day19/sol.py
@@ -1,6 +1,47 @@
-def distance(p1,p2):
-	return ((p2[0]-p1[0])**2+(p2[1]-p1[1])**2+(p2[2]-p1[2])**2)**(1/2)
-	# return (abs(p1[0]-p2[0]),abs(p1[1]-p2[1]),abs(p1[2]-p2[2]))
+from math import cos, floor, pi, sin
+
+import numpy as np
+
+def cosFloor(x):
+	return floor(cos(x))
+def sinFloor(x):
+	return floor(sin(x))
+
+def rotationMatrix(gamma,beta,alpha):
+	return [
+		[cosFloor(alpha)*cosFloor(beta),cosFloor(alpha)*sinFloor(alpha)*sinFloor(gamma)-sinFloor(alpha)*cosFloor(gamma),cosFloor(alpha)*sinFloor(beta)*cosFloor(gamma)+sinFloor(alpha)*sinFloor(gamma)],
+		[sinFloor(alpha)*cosFloor(beta),sinFloor(alpha)*sinFloor(alpha)*sinFloor(gamma)+cosFloor(alpha)*cosFloor(gamma),sinFloor(alpha)*sinFloor(beta)*cosFloor(gamma)-cosFloor(alpha)*sinFloor(gamma)],
+		[-sinFloor(beta)			   ,cosFloor(beta)*sinFloor(gamma) 												   ,cosFloor(beta)*cosFloor(gamma)]
+	]
+
+def mult(X,point):
+	result = [0,0,0]
+	result[0] = sum( [x*y for x,y in zip(X[0],point)])
+	result[1] = sum( [x*y for x,y in zip(X[1],point)])
+	result[2] = sum( [x*y for x,y in zip(X[2],point)])
+	if(len(set(result)) != len(result)):
+		return ()
+	for x in result:
+		if x not in point and -x not in point:
+			return ()
+	# print(result)
+	return result
+
+idx = 1
+def rotatePoint(x,y,z):
+	global idx
+	seen = set()
+	rotations = [0,90,180,270]
+	for i in rotations:
+		for j in rotations:
+			for k in rotations:
+				r = rotationMatrix(i*pi/180,j*pi/180,k*pi/180)
+				idx += 1
+				seen.add(tuple(mult(r,[x,y,z])))
+	for s in seen:
+		print(s)		
+	print(len(seen))
+	
 
 f = open("day19.test")
 content = f.read().split("\n\n")
@@ -12,20 +53,4 @@ for (i, scanner) in enumerate(content):
 		x, y, z = map(int, pt.split(","))
 		scanners[i].add((x,y,z))
 
-distances = set()
-
-for sc in scanners:
-	for x in sc:
-		print(x)
-		for y in sc:
-			if(x != y):
-				distances.add(distance(x, y))
-sz = len(distances) * 2
-for i in range(1000):
-	print(i," " ,i*(i-1)," ",sz)
-	if(i*(i-1) > sz):
-		break 
-
-# n(n-1) / 2 = 1419
-# n^2 - n - 2838 = 0
-#  d=b^2-4ac
+rotatePoint(3,1,2)
