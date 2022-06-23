debd68e966936a3e2960b2de6f90a1e9770d9384
Horjea Cosmin <horjeacosmin@gmail.com>
2022-06-23 22:54:18 +0300

    finish day 2

--- 2016/day2/day2.go
+++ 2016/day2/day2.go
@@ -18,33 +18,73 @@ func main() {
 	if err != nil {
 		log.Fatalf("Unable to open file")
 	}
-	position := Point{1, 1}
+
+	disallowed := []Point{{0, 0}, {0, 1}, {0, 3}, {0, 4}, {1, 0}, {1, 4}, {3, 0}, {3, 4}, {4, 0}, {4, 1}, {4, 3}, {4, 4}}
+	m := make(map[Point]string)
+
+	m[Point{2, 0}] = "1"
+	m[Point{1, 1}] = "2"
+	m[Point{2, 1}] = "3"
+	m[Point{3, 1}] = "4"
+	m[Point{0, 2}] = "5"
+	m[Point{1, 2}] = "6"
+	m[Point{2, 2}] = "7"
+	m[Point{3, 2}] = "8"
+	m[Point{4, 2}] = "9"
+	m[Point{1, 3}] = "A"
+	m[Point{2, 3}] = "B"
+	m[Point{3, 3}] = "C"
+	m[Point{2, 4}] = "D"
+
+	position := Point{0, 2}
 	data := strings.Split(string(body), "\n")
 	for _, line := range data {
 		for _, crr := range string(line) {
+
+			next_position := position
+
 			switch string(crr) {
 			case "D":
-				position.y += 1
+				next_position.y += 1
 			case "U":
-				position.y -= 1
+				next_position.y -= 1
 			case "R":
-				position.x += 1
+				next_position.x += 1
 			case "L":
-				position.x -= 1
+				next_position.x -= 1
 			}
+			ok := 1
+			for _, b := range disallowed {
+				if b == next_position {
+					ok = 0
+					break
+				}
+			}
+
+			if ok == 0 {
+				continue
+			}
+
+			position = next_position
+
 			if position.x < 0 {
 				position.x = 0
 			}
-			if position.x > 2 {
-				position.x = 2
+
+			if position.x > 4 {
+				position.x = 4
 			}
+
 			if position.y < 0 {
 				position.y = 0
 			}
-			if position.y > 2 {
-				position.y = 2
+
+			if position.y > 4 {
+				position.y = 4
 			}
+			// fmt.Println(position)
 		}
-		fmt.Printf("%d", 3*position.y+position.x+1)
+		// fmt.Println(position)
+		fmt.Printf("%s", string(m[position]))
 	}
 }
