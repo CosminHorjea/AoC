74fb98127db467de78766b9547bbb7bb50bda821
Horjea Cosmin <horjeacosmin@gmail.com>
2022-12-04 12:08:29 +0000

    day 4 initial solution

---
+++ 2022/day4/day4.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type Pair struct {
+	left  int
+	right int
+}
+
+func main() {
+	filename := "input.txt"
+	var testFlag = flag.Bool("test", false, "Run test")
+	flag.Parse()
+	if *testFlag {
+		filename = "input_test.txt"
+	}
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	// Part 1
+	// total := 0
+	// lines := strings.Split(string(data), "\n")
+	// for _, line := range lines {
+	// 	sections := strings.Split(line, ",")
+	// 	first_section := strings.Split(sections[0], "-")
+	// 	first_pair := toPair(first_section)
+	// 	second_section := strings.Split(sections[1], "-")
+	// 	second_pair := toPair(second_section)
+	// 	if first_pair.Contains(second_pair) || second_pair.Contains(first_pair) {
+	// 		total++
+	// 	}
+	// }
+	// fmt.Println(total)
+
+	// part2
+	total := 0
+	lines := strings.Split(string(data), "\n")
+	for _, line := range lines {
+		sections := strings.Split(line, ",")
+		first_section := strings.Split(sections[0], "-")
+		second_section := strings.Split(sections[1], "-")
+
+		first_pair := toPair(first_section)
+		second_pair := toPair(second_section)
+
+		if first_pair.Overlaps(second_pair) {
+			total++
+		}
+	}
+	fmt.Println(total)
+}
+
+func toPair(section []string) Pair {
+	left, _ := strconv.Atoi(section[0])
+	right, _ := strconv.Atoi(section[1])
+	return Pair{left, right}
+}
+
+func (p Pair) Contains(other_pair Pair) bool {
+	return p.left <= other_pair.left && p.right >= other_pair.right
+}
+
+func (p Pair) Overlaps(other_pair Pair) bool {
+	fmt.Println("check: ", p, other_pair)
+	if p.left > other_pair.right {
+		return false
+	}
+	if p.right < other_pair.left {
+		return false
+	}
+	return true
+}
