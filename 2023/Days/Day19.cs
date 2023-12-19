2b8f98af8e12f18b543a886688edc781f5153b6e
Cosmin Horjea <horjeacosmin@gmail.com>
2023-12-19 22:29:44 +0200

    day 19 part 2

--- 2023/Days/Day19.cs
+++ 2023/Days/Day19.cs
@@ -1,3 +1,4 @@
+using System.Numerics;
 using System.Runtime.InteropServices.Marshalling;
 using System.Security.Cryptography.X509Certificates;
 using System.Text.Json;
@@ -103,6 +104,92 @@ class Day19 : Solution
 
     public string Part2()
     {
-        throw new NotImplementedException();
+        Dictionary<string, List<(string, string)>> map = new Dictionary<string, List<(string, string)>>();
+        var contents = File.ReadAllLines("Inputs/Day19.in");
+        var rules = contents.TakeWhile(c => !string.IsNullOrEmpty(c)).ToList();
+        BigInteger ans = 0;
+
+        foreach (var rule in rules)
+        {
+            var label = rule.Split("{")[0];
+            map.Add(label, new List<(string, string)>());
+            Regex patters = new Regex(@"\{.*?\}");
+            var matches = patters.Match(rule);
+            var conditions = matches.Value.Trim(['{', '}']).Split(',').ToList();
+            for (int i = 0; i < conditions.Count; i++)
+            {
+                if (conditions[i].Contains(':'))
+                {
+                    var ineq = conditions[i].Split(':')[0];
+                    var dest = conditions[i].Split(':')[1];
+                    map[label].Add((ineq, dest));
+                }
+                else
+                {
+                    map[label].Add(("_", conditions[i]));
+                }
+            }
+        }
+
+        var values = new List<(int, int)> { (1, 4000), (1, 4000), (1, 4000), (1, 4000) };
+        Dictionary<char, int> indices = new() { { 'x', 0 }, { 'm', 1 }, { 'a', 2 }, { 's', 3 } };
+        Queue<(string, List<(int, int)>)> q = new Queue<(string, List<(int, int)>)>();
+
+        q.Enqueue(("in", values));
+        while (q.Count > 0)
+        {
+            var curr = q.Dequeue();
+            if (curr.Item1 == "A")
+            {
+                BigInteger a = 1;
+                foreach (var interval in curr.Item2)
+                {
+                    Console.Write($"{interval.Item1},{interval.Item2}|");
+                    a *= interval.Item2 - interval.Item1 + 1;
+                }
+                Console.WriteLine();
+                ans += a;
+                continue;
+            }
+            if (curr.Item1 == "R")
+            {
+                continue;
+            }
+            var labelRules = map[curr.Item1];
+            var remainingIntervals = new List<(int, int)>(curr.Item2);
+            foreach (var rule in labelRules)
+            {
+                var next_intervals = new List<(int, int)>(remainingIntervals);
+                if (rule.Item1 == "_")
+                {
+                    q.Enqueue((rule.Item2, remainingIntervals));
+                }
+                else if (rule.Item1.Contains(">"))
+                {
+                    var num = int.Parse(rule.Item1.Split('>')[1]);
+                    var idx = indices[rule.Item1[0]];
+                    next_intervals[idx] = (num + 1, remainingIntervals[idx].Item2);
+                    remainingIntervals[idx] = (remainingIntervals[idx].Item1, num);
+                    q.Enqueue((rule.Item2, new List<(int, int)>(next_intervals)));
+                }
+                else if (rule.Item1.Contains("<"))
+                {
+                    var num = int.Parse(rule.Item1.Split('<')[1]);
+                    var idx = indices[rule.Item1[0]];
+                    next_intervals[idx] = (remainingIntervals[idx].Item1, num - 1);
+                    remainingIntervals[idx] = (num, remainingIntervals[idx].Item2);
+                    q.Enqueue((rule.Item2, new List<(int, int)>(next_intervals)));
+                }
+            }
+        }
+        return $"{ans}";
     }
 }
+// test
+// 167409079868000 - correct
+// 104624836840500
+// 104624836840500
+// 163494853788000
+
+// input
+// 126226266150329 too high
