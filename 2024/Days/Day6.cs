d9d7e43d40ad68411494698dcaef5781373fb7f7
CosminHorjea <horjeacosmin@gmail.com>
2024-12-07 22:39:39 +0200

    day 6 part 2

--- 2024/Days/Day6.cs
+++ 2024/Days/Day6.cs
@@ -50,21 +50,71 @@ class Day6 : Solution
 
     public string Part2()
     {
-        var manageCompanyIds = new Dictionary<int, MonthlyUsageFullRecordDto>();
-        // manageCompanyIds[1] = new MonthlyUsageFullRecordDto
-        // {
-        //     ManageCompanyId = 1,
-        //     GravityZoneCompanyId = "1",
-        //     EndpointMonthlyUsage = 1
-        // };
+        var map = File.ReadAllLines("Inputs\\Day6.in");
+        var answer = 0;
+        int n = map.Length;
+        int m = map[0].Length;
+        HashSet<(int, int)> obstacles = new HashSet<(int, int)>();
+        (int, int) explorer = (0, 0);
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                switch (map[i][j])
+                {
+                    case '#':
+                        obstacles.Add((i, j));
+                        break;
+                    case '^':
+                        explorer = (i, j);
+                        break;
+                }
+            }
+        }
 
-        Console.WriteLine(manageCompanyIds[1]);
-        return "";
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < m; j++)
+            {
+                var newObstacles = new HashSet<(int, int)>(obstacles);
+                newObstacles.Add((i, j));
+                if (explorer != (i, j) && map[i][j] != '#' && IsLooping(n, m, newObstacles, explorer))
+                {
+                    answer++;
+                }
+            }
+        }
+
+        return "" + answer;
     }
-    public class MonthlyUsageFullRecordDto
+
+    private static bool IsLooping(int n, int m, HashSet<(int, int)> obstacles, (int, int) explorer)
     {
-        public int ManageCompanyId { get; set; }
-        public string GravityZoneCompanyId { get; set; }
-        public int EndpointMonthlyUsage { get; set; }
+        HashSet<((int, int), int)> visited = new HashSet<((int, int), int)>();
+        List<(int, int)> directions = new List<(int, int)> { (-1, 0), (0, 1), (1, 0), (0, -1) };
+        var currentDirection = 0;
+        visited.Add((explorer, currentDirection));
+
+        while (true)
+        {
+            var next = (explorer.Item1 + directions[currentDirection].Item1, explorer.Item2 + directions[currentDirection].Item2);
+            if (obstacles.Contains(next))
+            {
+                currentDirection = (currentDirection + 1) % 4;
+                continue;
+            }
+            if (next.Item1 < 0 || next.Item1 >= n || next.Item2 < 0 || next.Item2 >= m)
+            {
+                break;
+            }
+            if (visited.Contains((next, currentDirection)))
+            {
+                return true;
+            }
+            visited.Add((next, currentDirection));
+            explorer = next;
+        }
+
+        return false;
     }
 }
